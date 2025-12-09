0dc92cfc2b77d481b6322d9a4f91c11a76b900ad
ggonei <george.oneill@ess.eu>
2025-12-09 22:44:50 +0100

    theory passes the test

--- day9/day9_p2.cbl
+++ day9/day9_p2.cbl
@@ -22,6 +22,12 @@
 
            WORKING-STORAGE SECTION.
              01 eofile PIC 9(1) VALUE 0.
+             01 bigside PIC 9(5) VALUE 0.
+             01 bigx PIC 9(5) VALUE 0.
+             01 bigy PIC 9(5) VALUE 0.
+             01 posbx PIC s9(5) VALUE 0.
+             01 posby PIC s9(5) VALUE 0.
+             01 posf PIC s9(5) VALUE 0.
              01 cnt PIC 9(5) VALUE 0.
              01 idx PIC 9(5) VALUE 0.
              01 idx2 PIC 9(5) VALUE 0.
@@ -45,16 +51,75 @@
                  posiy(cnt)
            END-PERFORM.
            CLOSE inputfile.
-           PERFORM VARYING idx FROM 1 BY 1 UNTIL idx > cnt
-             PERFORM VARYING idx2 FROM 1 BY 1 UNTIL idx2 > cnt
-               DISPLAY idx "," idx2
+           PERFORM VARYING idx FROM 2 BY 1 UNTIL idx > cnt
+             IF FUNCTION ABS(posix(idx - 1) - posix(idx)) > bigx THEN
+               MOVE FUNCTION ABS(posix(idx - 1) - posix(idx)) TO bigx
+               COMPUTE posbx = idx - 1
+             END-IF
+             IF FUNCTION ABS(posiy(idx - 1) - posiy(idx)) > bigy THEN
+               MOVE FUNCTION ABS(posiy(idx - 1) - posiy(idx)) TO bigy
+               COMPUTE posby = idx - 1
+             END-IF
+           END-PERFORM.
+      *    IF bigx > bigy THEN
+      *      MOVE posbx TO posf
+      *      MOVE FUNCTION ABS(posix(posbx) - posix(posbx + 1) +1)
+      *      TO bigside
+      *    ELSE IF bigy > bigx THEN
+      *        MOVE posby TO posf
+      *        MOVE FUNCTION ABS(posiy(posby) - posiy(posby + 1) +1)
+      *        TO bigside
+      *      ELSE
+      *        DISPLAY "Looks like you have a square"
+      *      END-IF
+      *    END-IF
+           DISPLAY "Testing with " posbx "," posby
+             PERFORM VARYING idx FROM 1 BY 2 UNTIL idx > cnt
                COMPUTE rect =
-                (FUNCTION ABS(posix(idx) - posix(idx2)) + 1) *
-                (FUNCTION ABS(posiy(idx) - posiy(idx2)) + 1)
+                (FUNCTION ABS(posix(idx) - posix(posbx)) + 1) *
+                (FUNCTION ABS(posiy(idx) - posiy(posbx)) + 1)
                IF rect > rectbiggest THEN
                  DISPLAY "THIS RECTANGLE IS " rect
+                 DISPLAY
+                 "(" posix(idx) "," posiy(idx) "),"
+                 "(" posix(posbx) "," posiy(idx) "),"
+                 "(" posix(posbx) "," posiy(posbx) "),"
+                 "(" posix(idx) "," posiy(posbx) ")"
+                 DISPLAY " " posix(idx + 1) "," posiy(idx + 1)
+                 DISPLAY " " posix(idx - 1) "," posiy(idx - 1)
+                 DISPLAY " " posix(posbx + 1) "," posiy(posbx + 1)
+                 DISPLAY " " posix(posbx - 1) "," posiy(posbx - 1)
                  MOVE rect TO rectbiggest
                END-IF
-             END-PERFORM
+      *        IF posix(idx) = posix(posbx)
+      *        AND posix(idx + 1) = posix(posbx + 1)
+      *        THEN DISPLAY "SuccessX: " idx
+      *        IF posiy(idx) = posiy(posby)
+      *        AND posiy(idx + 1) = posiy(posby + 1)
+      *        THEN DISPLAY "SuccessY: " idx
            END-PERFORM.
            STOP RUN.
+      *      COMPUTE rect =
+      *       (FUNCTION ABS(posix(idx) - posix(idx2)) + 1) *
+      *       (FUNCTION ABS(posiy(idx) - posiy(idx2)) + 1)
+      *      IF rect > rectbiggest THEN
+      *        DISPLAY "THIS RECTANGLE IS " rect
+      *        DISPLAY
+      *        "(" posix(idx) "," posiy(idx) "),"
+      *        "(" posix(idx2) "," posiy(idx) "),"
+      *        "(" posix(idx2) "," posiy(idx2) "),"
+      *        "(" posix(idx) "," posiy(idx2) ")"
+      *        IF posix(idx) > posix(idx2) THEN
+      *          IF posiy(idx) > posiy(idx2) THEN
+
+      *          ELSE
+
+      *          END-IF
+      *        ELSE
+      *        END-IF
+      *        DISPLAY " " posix(idx + 1) "," posiy(idx + 1)
+      *        DISPLAY " " posix(idx - 1) "," posiy(idx - 1)
+      *        DISPLAY " " posix(idx2 + 1) "," posiy(idx2 + 1)
+      *        DISPLAY " " posix(idx2 - 1) "," posiy(idx2 - 1)
+      *        MOVE rect TO rectbiggest
+      *      END-IF
