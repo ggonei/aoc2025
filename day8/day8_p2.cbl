712592a63e27f2b3598203db85a78f5b724341a3
ggonei <george.oneill@ess.eu>
2025-12-09 17:01:06 +0100

    day8 completed

--- day8/day8_p2.cbl
+++ day8/day8_p2.cbl
@@ -1,5 +1,5 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. day8-p1.
+       PROGRAM-ID. day8-p2.
 
        ENVIRONMENT DIVISION.
            INPUT-OUTPUT SECTION.
@@ -28,7 +28,6 @@
             01 idx2 PIC s9(6) VALUE 0.
             01 starti PIC s9(6) VALUE 0.
             01 numitems PIC s9(7) VALUE 0.
-            01 pairs PIC s9(5) VALUE 1000.
             01 tmpstring PIC X(1001) VALUE SPACES.
             01 positions.
              02 place OCCURS 100001 TIMES.
@@ -74,72 +73,17 @@
            DISPLAY "numitems:" numitems ",pairs:" closest ";" hdsmallest
            MOVE 0 TO idx
            SORT pidx ON ASCENDING KEY distance.
-           PERFORM VARYING idx FROM 1 BY 1 UNTIL idx > pairs
-            PERFORM VARYING idx2 FROM 1 BY 1 UNTIL idx2 > idx
-             IF (origpos(idx) = origpos(idx2)
-             OR origpos(idx) = neighbour(idx2)
-             OR neighbour(idx) = origpos(idx2)
-             OR neighbour(idx) = neighbour(idx2))
-             THEN
-              IF circuit(idx) > circuit(idx2) OR circuit(idx) = 0 THEN
-               MOVE circuit(idx2) TO circuit(idx)
-              END-IF
-             END-IF
-            END-PERFORM
-            IF circuit(idx) = 0 THEN
-             ADD 1 TO circuitn
-             MOVE circuitn TO circuit(idx)
-            END-IF
-           END-PERFORM.
-           PERFORM VARYING starti FROM 1 BY 1 UNTIL starti > 19
-           DISPLAY starti
-           PERFORM VARYING idx FROM 1 BY 1 UNTIL idx > pairs
-            PERFORM VARYING idx2 FROM 1 BY 1 UNTIL idx2 > pairs
-             IF (origpos(idx) = origpos(idx2)
-             OR origpos(idx) = neighbour(idx2)
-             OR neighbour(idx) = origpos(idx2)
-             OR neighbour(idx) = neighbour(idx2))
-             THEN
-              IF circuit(idx) > circuit(idx2) OR circuit(idx) = 0 THEN
-               DISPLAY pidx(idx) ", " pidx(idx2)
-               MOVE circuit(idx2) TO circuit(idx)
-              END-IF
-             END-IF
-            END-PERFORM
-           END-PERFORM
-           END-PERFORM.
-           SORT pidx ON DESCENDING KEY circuit.
-           PERFORM VARYING starti FROM 1 BY 1 UNTIL starti > pairs + 1
+           PERFORM VARYING starti FROM 1 BY 1 UNTIL cnt = numitems
            DISPLAY pidx(starti)
-            IF circuit(starti) NOT= circuit(starti - 1) THEN
-             MOVE 0 TO cnt
-             INSPECT tmpstring TALLYING cnt FOR ALL "X"
-             DISPLAY cnt
-             IF ans1 < cnt THEN
-              MOVE ans2 TO ans3
-              MOVE ans1 TO ans2
-              MOVE cnt TO ans1
-             ELSE IF ans2 < cnt THEN
-               MOVE ans2 TO ans3
-               MOVE cnt TO ans2
-              ELSE IF ans3 < cnt THEN
-                MOVE cnt TO ans3
-               END-IF
-              END-IF
-             END-IF
-             MOVE SPACES TO tmpstring
-             MOVE "X" TO tmpstring(origpos(starti):1)
-             MOVE "X" TO tmpstring(neighbour(starti):1)
-             DISPLAY FUNCTION TRIM(tmpstring)
-            ELSE
-             MOVE "X" TO tmpstring(origpos(starti):1)
-             MOVE "X" TO tmpstring(neighbour(starti):1)
-             DISPLAY FUNCTION TRIM(tmpstring)
+            MOVE 0 TO cnt
+            MOVE "X" TO tmpstring(origpos(starti):1)
+            MOVE "X" TO tmpstring(neighbour(starti):1)
+            DISPLAY FUNCTION TRIM(tmpstring, TRAILING)
+            INSPECT tmpstring TALLYING cnt FOR LEADING "X"
+            IF cnt = numitems THEN
+             COMPUTE hd = posi(origpos(starti), 1)
+              * posi(neighbour(starti), 1)
             END-IF
            END-PERFORM.
-           DISPLAY ans3.
-           DISPLAY ans2.
-           DISPLAY ans1.
-           COMPUTE hd = ans1 * ans2 * ans3.
            DISPLAY hd.
            STOP RUN.
